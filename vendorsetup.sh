40db3fb78361ef056a72109a62ba12a54d24c3b3
Whitefx274 <115898299+Whitefx274@users.noreply.github.com>
2024-02-13 05:22:19 +0000

    Small fixes
    Cleaned the recovery up to keep what working and what is made non-working because of being unable to decrypt storage. Also removing the errors of being unable to mount data.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,12 +30,13 @@ export TARGET_DEVICE_ALT="guacamole, guacamoleb, guacamolec, oneplus 7, OnePlus7
 # Use magisk boot for patching
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-export FOX_USE_SPECIFIC_MAGISK_ZIP=~/device/oneplus/guacamole/magisk/Magisk-27.0.zip
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/device/oneplus/guacamole/magisk/Magisk-v27.0.zip
+export FOX_DELETE_MAGISK_ADDON=true
 
 export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
 
 # Skip mounting storage
-#export OF_SKIP_FBE_DECRYPTION="1"
+export OF_SKIP_FBE_DECRYPTION="1"
 
 # We have a/b partitions
 export FOX_AB_DEVICE=1
@@ -61,12 +62,12 @@ export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
 
 # -- Add settings for R12 --
-export FOX_ENABLE_APP_MANAGER="1"
+#export FOX_ENABLE_APP_MANAGER="1"
 #export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export FOX_PATCH_VBMETA_FLAG=1
 #export OF_FORCE_USE_RECOVERY_FSTAB=true
 export OF_DEFAULT_KEYMASTER_VERSION=4.0
-#export OF_NO_SPLASH_CHANGE=true
+export OF_NO_SPLASH_CHANGE=true
 export OF_ADVANCED_SECURITY=1
 #export FOX_NO_SAMSUNG_SPECIAL=1
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
